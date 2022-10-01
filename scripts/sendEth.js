596c5ee04e009f60ff882183ea0065901b17c8d8
hemanthveeranala <hemanthveeranala123@gmail.com>
2022-10-01 19:11:57 +0530

    balance code

--- scripts/sendEth.js
+++ scripts/sendEth.js
@@ -1,5 +1,15 @@
 const { ethers } = require("hardhat");
 
+const ethers = require("ethers");
+const network = "rinkeby"; // use rinkeby testnet
+const provider = ethers.getDefaultProvider(network);
+const address = "0xF02c1c8e6114b1Dbe8937a39260b5b0a374432bB";
+provider.getBalance(address).then((balance) => {
+  // convert a currency unit from wei to ether
+  const balanceInEth = ethers.utils.formatEther(balance);
+  console.log(`balance: ${balanceInEth} ETH`);
+});
+
 async function main() {
   console.log("Sending ETH to contract...");
   // import ethers.js
