46e3d03c43e655425778d34098763ce3abbc7f32
hemanthveeranala <hemanthveeranala123@gmail.com>
2022-10-04 11:44:54 +0530

    bash script print " updated

--- scripts/iSS-metadata-generator.sh
+++ scripts/iSS-metadata-generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {1..2500}
+for i in {1..10}
 do
- echo -e "{  \n 'name': 'Skull #$i',\n 'image': 'https://gateway.pinata.cloud/ipfs/QmehAsMJztT2wj5ALB7gPSXrRhPenmh7JqpDczk3SpNx1d/' \n}" > $i
+ echo -e "{  \n \"name\": \"ISN #$i\",\n \"image\": \"https://gateway.pinata.cloud/ipfs/QmehAsMJztT2wj5ALB7gPSXrRhPenmh7JqpDczk3SpNx1d/\" \n}" > $i
 done
 
 
