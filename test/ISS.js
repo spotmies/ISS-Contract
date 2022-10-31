30147e51edcd5e05ed9cd9ac6531d6f1fad727d7
hemanthveeranala <hemanthveeranala123@gmail.com>
2022-10-31 15:46:54 +0530

    INItial screech

--- test/ISS.js
+++ test/ISS.js
@@ -1,12 +1,13 @@
 describe("Deploy contract", function () {
   it("should deploy a contract", async function () {
-    const contract = await ethers.getContractFactory("skullSyndicate");
-    const skull = await contract.deploy(
-      750,
-      "nothing",
-      "0xecefeddaa6971ceee00c6214adbbbc46b26fc29fdc85702b76dd516fadf4b47f",
-      "0xecefeddaa6971ceee00c6214adbbbc46b26fc29fdc85702b76dd516fadf4b47f"
-    );
+    const contract = await ethers.getContractFactory("Screech");
+    const skull = await contract
+      .deploy
+      // 750,
+      // "nothing",
+      // "0xecefeddaa6971ceee00c6214adbbbc46b26fc29fdc85702b76dd516fadf4b47f",
+      // "0xecefeddaa6971ceee00c6214adbbbc46b26fc29fdc85702b76dd516fadf4b47f"
+      ();
     await skull.deployed();
     console.log("Lock deployed to:", skull.address);
   });
